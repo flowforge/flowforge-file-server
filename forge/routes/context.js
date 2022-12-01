741330b4fb4637cf8308b680586b2a591e3dbcd2
Ben Hardill <ben@flowforge.com>
2022-12-01 10:03:42 +0000

    Support multi key set/get

--- forge/routes/context.js
+++ forge/routes/context.js
@@ -17,42 +17,70 @@ module.exports = async function (app, opts, done) {
     /**
      * Create/Update key
      *
-     * @name /v1/context/:projectId/:scope/:key
+     * @name /v1/context/:projectId/:scope
      * @static
      * @memberof forge.fileserver.context
      */
-    app.post('/:projectId/:scope/:key', {
-
+    app.post('/:projectId/:scope', {
+        schema: {
+            body: {
+                type: 'array'
+            }
+        }
     }, async (request, reply) => {
         const body = request.body
-        const key = request.params.projectId +
+        body.array.forEach(element => {
+            const key = request.params.projectId +
             '.' + request.params.scope +
-            '.' + request.params.key
-        util.setObjectProperty(store, key, body)
+            '.' + element.key
+            util.setObjectProperty(store, key, element.value)
+        })
         reply.code(200).send({})
     })
 
     /**
      * Get key
      *
-     * @name /v1/context/:projectId/:scope/:key
+     * @name /v1/context/:projectId/:scope?
      * @static
      * @memberof forge.fileserver.context
      */
-    app.get('/:projectId/:scope/:key', {
-
-    }, async (request, reply) => {
-        const key = request.params.projectId +
-            '.' + request.params.scope +
-            '.' + request.params.key
-        try {
-            const value = util.getObjectProperty(store, key)
-            reply.send(value)
-        } catch (err) {
-            if (err.type === 'TypeError') {
-                reply.code(404).send()
+    app.get('/:projectId/:scope/', {
+        schema: {
+            query: {
+                type: 'object',
+                properties: {
+                    key: {
+                        type: 'array',
+                        items: { type: 'string' }
+                    }
+                },
+                required: ['key']
             }
         }
+    }, async (request, reply) => {
+        const keys = request.query.keys
+        const values = []
+        keys.forEach(key => {
+            const fullkey = request.params.projectId +
+            '.' + request.params.scope +
+            '.' + key
+            try {
+                const value = util.getObjectProperty(store, fullkey)
+                const ret = {
+                    key,
+                    value
+                }
+                values.push(ret)
+            } catch (err) {
+                if (err.type === 'TypeError') {
+                    // reply.code(404).send()
+                    values[key] = undefined
+                }
+                // return false
+            }
+        })
+        reply.send(values)
     })
 
     /**
@@ -62,7 +90,7 @@ module.exports = async function (app, opts, done) {
      * @static
      * @memberof forge.fileserver.context
      */
-    app.get('/:projectId/:scope', {
+    app.get('/:projectId/:scope/keys', {
 
     }, async (request, reply) => {
         const key = request.params.projectId +
