cbc55b77d20160d3b2fcca4f4c18636c4856f63c
Ben Hardill <ben@flowforge.com>
2022-12-02 20:14:22 +0000

    First pass at redis driver

--- forge/context-driver/redis.js
+++ forge/context-driver/redis.js
@@ -1,6 +1,35 @@
 const redis = require('redis')
+const util = require('@node-red/util').util
 
 let client
+
+async function recursiveInsert (projectId, scope, path, value) {
+    try {
+        const count = await client.json.objLen(projectId, `$.${scope}.${path}`)
+        if (count.length) {
+            const existing = (await client.json.get(projectId, { path: [`$.${scope}.${path}`] }))[0]
+            Object.assign(existing, value)
+            await client.json.set(projectId, `$.${scope}.${path}`, existing, { XX: true })
+        } else {
+            const response = await client.json.set(projectId, `$.${scope}.${path}`, value, { XX: true })
+            if (!response) {
+                const parts = path.split('.')
+                const top = parts.pop()
+                const shortPath = parts.join('.')
+                const wrappedValue = {}
+                util.setObjectProperty(wrappedValue, top, value)
+                if (parts.length !== 0) {
+                    await recursiveInsert(projectId, scope, shortPath, wrappedValue)
+                } else {
+                    await client.json.set(projectId, `$.${scope}.${top}`, value)
+                }
+            }
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 module.exports = {
     init: async function (options) {
         client = redis.createClient({
@@ -9,16 +38,29 @@ module.exports = {
         await client.connect()
     },
     set: async function (projectId, scope, input) {
-        if (!await client.json.objlen(projectId, '$')) {
-            await client.json.set(projectId, '$', {})
-            await client.json.set(projectId, '$.' + scope, {})
-        }
-        for (const element of input) {
-            const key = '$.' + scope + '.' + element.key
-            await client.json.set(projectId, key, element.value)
-        }
+        input.forEach(element => {
+            recursiveInsert(projectId, scope, element.key, element.value)
+        })
     },
-    get: async function (projectId, scope, keys) {},
+    get: async function (projectId, scope, keys) {
+        const values = []
+        const paths = []
+        keys.forEach(key => {
+            paths.push(`${scope}.${key}`)
+        })
+        const response = await client.json.get(projectId, { path: paths })
+        Object.keys(response).forEach(k => {
+            const parts = k.split('.')
+            parts.shift()
+            const key = parts.join('.')
+            const value = test[k][0]
+            values.push({
+                key,
+                value
+            })
+        })
+        return values
+    },
     keys: async function (projectId, scope) {
         const keys = await client.json.objkeys(projectId, '$.' + scope)
         return keys
