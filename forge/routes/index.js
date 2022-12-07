df69fc077e99129c1b37b696f5762d2ad7fc43ae
Ben Hardill <ben@flowforge.com>
2022-12-07 11:49:16 +0000

    Update forge/routes/index.js
    Co-authored-by: Stephen McLaughlin <44235289+Steve-Mcl@users.noreply.github.com>

--- forge/routes/index.js
+++ forge/routes/index.js
@@ -4,7 +4,7 @@ module.exports = fp(async function (app, opts, done) {
     app.addHook('preHandler', app.checkAuth)
     await app.register(require('./files'), { prefix: '/v1/files', logLevel: app.config.logging.http })
     await app.register(require('./quota'), { prefix: '/v1/quota', logLevel: app.config.logging.http })
-    if (app.config.context) {
+    if (app.config.context?.type) {
         await app.register(require('./context'), { prefix: '/v1/context', logLevel: app.config.logging.http })
     }
     done()
